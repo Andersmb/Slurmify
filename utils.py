c05451207f49f43ed4a5a7fb6042d0b151bc7e86
Andersmb <anders.brakestad@gmail.com>
2020-01-28 11:03:06 +0100

    fixed ambig. redirect when executing g16

--- utils.py
+++ utils.py
@@ -252,12 +252,11 @@ def gaussian_job(inputfile=None, outputfile=None, is_dev=None, slurm_account=Non
         else:
             print(f"Warning: Copy of .chk file requested, but the file does not exist ({inputfile+'.chk'}). Continuing without copying file.")
 
-
     # Execute Gaussian
     jobfile.append("")
     jobfile.append(f"cd $SCRATCH")
     if cluster == "stallo": jobfile.append(f"G09.prep.slurm {inputfile}")
-    jobfile.append(f"time g16.ib < {inputfile+extension_inputfile} > {outputfile+extension_outputfile}")
+    jobfile.append(f"time g16.ib {inputfile+extension_inputfile} > {outputfile+extension_outputfile}")
     jobfile.append("")
 
     # Copy back files
