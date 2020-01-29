8877f974d7c19c527f578b511e5164ed68518ee9
Andersmb <anders.brakestad@gmail.com>
2020-01-29 14:41:13 +0100

    removed check of number of nodes on fram

--- slurmify.py
+++ slurmify.py
@@ -111,8 +111,6 @@ args = parser.parse_args()
 # Sort out some things
 if args.output is None: args.output = args.input
 if args.account is None: args.account = ACCOUNTS[cluster]
-if cluster == "fram" and int(args.nodes) < 4 and not args.dev:
-    sys.exit("4 nodes is the minimum node-count on fram for non-dev jobs")
 
 # Evaluate whether the destination exists, and ask for permission to create if
 if not os.path.isdir(args.destination):
