a8c894310d1ee302778fa6a22913fb488c2609f7
Andersmb <anders.brakestad@gmail.com>
2020-04-29 09:42:49 +0200

    updated to orca 4.2.1 on stallo

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ vars = {
         "mpi_version": "OpenMPI/3.1.3-GCC-8.2.0-2.31.1",
         "gaussian_version": "Gaussian/g16_B.01",
         "scratch": f"/global/work/ambr/${{SLURM_JOBID}}",
-        "path_orca": f"/home/ambr/software/orca_4_1_2_linux_x86-64_openmpi313/orca",
+        "path_orca": f"/home/ambr/software/orca_4_2_1_linux_x86-64_openmpi314/orca",
         "path_mpi": "/global/hds/software/cpu/eb3/OpenMPI/3.1.3-GCC-8.2.0-2.31.1/bin",
         "path_mrchem": "/home/ambr/mrchem_master200303/build/bin",
         "mrchem_venv": "/home/ambr/.local/share/virtualenvs/mrchem_master200303-X6sXxlx6/bin/activate",
